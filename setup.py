388fcbe2df4c1749e196b83e6203f1f0193943a4
Ritik1507 <112415038+Ritik1507@users.noreply.github.com>
2024-03-22 15:45:16 +0000

    file updated

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-Implementation"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Breast_cancer"
+AUTHOR_USER_NAME = "ritik1507"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "ritikpadhi1507@gmail.com"
 
 
 setuptools.setup(
